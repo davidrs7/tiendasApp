485983308b0e670c7b9a64040fa0e7927d542ed9
davidrs7 <davidrodriguezrivera0@gmail.com>
2022-03-01 18:22:01 -0500

    Ajustes finales para proximo desplieuge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './components/about/about.component';
-import { HeroesComponent } from './components/heroes/heroes.component'; 
 import { HomeComponent } from './components/home/home.component'; 
-import { HeroeComponent } from './components/heroe/heroe.component';
-import { BuscarHeroeComponent } from './components/buscar-heroe/buscar-heroe.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { ProductoTarjetaComponent } from './components/producto-tarjeta/producto-tarjeta.component';
@@ -16,7 +13,6 @@ import { CarritoComponent } from './components/carrito/carrito.component';
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'buscar-heroe/:Texto', component: BuscarHeroeComponent },
     { path: 'categorias', component: CategoriasComponent},
     { path: 'inicio', component: InicioComponent},
     { path: 'medios-pago', component: MediosPagoComponent},
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesComponent } from '../heroes/heroes.component';
 
 
 @Component({
